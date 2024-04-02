a9b9361e0061a623c94e2ef4400257ad4693c811
vtertre <vincent.tertre@tiime.fr>
2024-04-02 21:38:03 +0200

    rendre transactionnelle l'exécution des handlers de commandes et de queries

--- src/main/java/vtertre/command/CommandHandler.java
+++ src/main/java/vtertre/command/CommandHandler.java
@@ -1,7 +1,9 @@
 package vtertre.command;
 
+import org.springframework.transaction.annotation.Transactional;
 import vtertre.infrastructure.bus.MessageHandler;
 
 public interface CommandHandler<TCommand extends Command<TResponse>, TResponse> extends MessageHandler<TCommand, TResponse> {
+    @Transactional
     TResponse execute(TCommand command);
 }
--- src/main/java/vtertre/query/QueryHandler.java
+++ src/main/java/vtertre/query/QueryHandler.java
@@ -1,7 +1,9 @@
 package vtertre.query;
 
+import org.springframework.transaction.annotation.Transactional;
 import vtertre.infrastructure.bus.MessageHandler;
 
 public interface QueryHandler<TQuery extends Query<TResponse>, TResponse> extends MessageHandler<TQuery, TResponse> {
+    @Transactional
     TResponse execute(TQuery query);
 }
